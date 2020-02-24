9907e7d547e237a1abe527539494e11acbe9788f
sunny-hwang <59826727+sunny-hwang@users.noreply.github.com>
2020-02-24 21:34:43 +0900

    Create 프로그래머스_문자열압축(케이스 한개 런타임).py

---
+++ 0225/프로그래머스_문자열압축(케이스 한개 런타임).py
@@ -0,0 +1,32 @@
+# 1개, 2개, 3개 .. 단위로 반복한다.
+# 단위별로 스택에 넣고 문자열과 비교한다.
+
+def solution(s):
+    answer = []
+    size = len(s)
+
+    # 문자열 자를 단위
+    for i in range(1, size):
+        cnt = 1
+        temp_ans = []
+        temp_ans.append(s[0:i])
+
+        for j in range(i, size, i):
+            if j+i > size:
+                temp_ans.append(s[j:size])
+                break
+            else:
+                if temp_ans[-1] == s[j:j+i]:
+                    cnt += 1
+                else:
+                    if cnt > 1:
+                        temp_ans.append(str(cnt))
+                        cnt = 1
+                    temp_ans.append(s[j:j+i])
+
+        if cnt > 1:
+            temp_ans.append(str(cnt))
+        answer.append(len(''.join(temp_ans)))
+
+    #print(answer)
+    return min(answer)
