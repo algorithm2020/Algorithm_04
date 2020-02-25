767b3b3d21aedab89e5fc9f9fbbc93ad965cc4f1
sunny-hwang <59826727+sunny-hwang@users.noreply.github.com>
2020-02-25 22:16:21 +0900

    Update and rename 프로그래머스_등교길(실패).py to 프로그래머스_등교길(케이스2개실패).py

--- 0225/프로그래머스_등교길(케이스2개실패).py
+++ 0225/프로그래머스_등교길(케이스2개실패).py
@@ -1,17 +1,25 @@
 # (x,y)칸 까지의 경로수 = (x-1,y)칸 까지의 경로수 + (x,y-1)칸 까지의 경로수
 # 물 웅덩이는 지나가지 못한다.
 
+
 def solution(m, n, puddles):
     answer = 0
 
+    # 모서리가 웅덩이인 경우를 고려해야 됨!
     check = [[1]*m for _ in range(n)]
+
     for x, y in puddles:
         check[y-1][x-1] = 0
+        if x==1 and y<n:
+            check[y][x-1] = 0
+
 
     for i in range(1,n):
         for j in range(1,m):
-            if check[i][j] != 0:
-                check[i][j] = (check[i][j-1] + check[i-1][j])%1000000007
+            if check[i][j] == 0:
+                continue
+            check[i][j] = (check[i][j-1] + check[i-1][j])%1000000007
 
     answer = check[n-1][m-1]
     return answer
+
