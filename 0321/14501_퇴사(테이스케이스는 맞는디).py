508711862901e7bdccb2a00a4a956c22d0da7287
sunny-hwang <59826727+sunny-hwang@users.noreply.github.com>
2020-03-16 18:09:30 +0900

    Create 14501_퇴사(테이스케이스는 맞는디).py

---
+++ 0321/14501_퇴사(테이스케이스는 맞는디).py
@@ -0,0 +1,43 @@
+# d[i] : i일부터 상담을 했을 때 최대 수익
+
+
+ans = 0
+t = [0]
+p = [0]
+n = int(input())
+d = [0] * (n+1)
+for _ in range(n):
+    a, b = map(int, input().split())
+    t.append(a)
+    p.append(b)
+
+
+def dfs(i):
+
+    global d
+
+    if d[i] == 0:
+        if i+ t[i] <= n+1:
+            d[i] = p[i]
+        else:
+            return
+
+    ni = i + t[i]
+
+    if ni <= n:
+        if d[i] == 0:
+            dfs(ni)
+        elif d[ni] != 0:
+            d[i] += max(d[ni:])
+
+
+
+
+
+
+for i in range(n, 0, -1):
+
+    dfs(i)
+
+
+print(max(d))
